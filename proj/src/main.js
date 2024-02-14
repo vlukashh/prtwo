964c61172a3c6f4a5d1f50c285c6f658d672a347
vlukashh <ankalukashenko@gmail.ru>
2024-02-15 00:30:19 +0700

    добавленеи карточки

--- proj/src/main.js
+++ proj/src/main.js
@@ -18,5 +18,74 @@ let app = new Vue({
         inputOne: null,
         inputTwo: null,
         inputThr: null,
+    },
+    watch: {
+        firstColumn: {
+            handler(newFirstColumn) {
+                this.saveData();
+            },
+            deep: true
+        },
+        secondColumn: {
+            handler(newSecondColumn) {
+                this.saveData();
+            },
+            deep: true
+        },
+        thirdColumn: {
+            handler(newThirdColumn) {
+                this.saveData();
+            },
+            deep: true
+        }
+    },
+    methods: {
+        //сохранение данных
+        saveData() {
+            const data = {
+                firstColumn: this.firstColumn,
+                secondColumn: this.secondColumn,
+                thirdColumn: this.thirdColumn
+            };
+            localStorage.setItem(storageKey, JSON.stringify(data));
+        },
+        //обнолвение прогресса карточки
+        updateProgress(card) {
+            const checkedCount = card.items.filter(item => item.checked).length;
+            const progress = (checkedCount / card.items.length) * 100;
+            card.isComplete = progress === 100;
+            if (card.isComplete) {
+                card.lastChecked = new Date().toLocaleString();
+            }
+            this.checkMoveCard();
+        },
+
+        //проверка нужно ли перемещать карточку между столбцами
+        checkMoveCard() {
+            this.MoveFirstColm();
+            this.MoveSecondColm();
+        },
+        //добавление карточки в первый столбец
+        addCard() {
+            const newGroup = {
+                id: Date.now(),
+                groupName: this.groupName,
+                items: [
+                    { text: this.inputOne, checked: false },
+                    { text: this.inputTwo, checked: false },
+                    { text: this.inputThr, checked: false },
+                ]
+            }
+            if (this.firstColumn.length < 3) {
+                this.firstColumn.push(newGroup)
+            }
+            this.groupName = null,
+                this.inputOne = null,
+                this.inputTwo = null,
+                this.inputThr = null
+        }
+    },
+    mounted() {
+        // this.checkBlockColumn();
     }
 })
